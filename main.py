e969fa08b510c3e8f443422923bd51277f00e820
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-07-10 02:28:30 +0200

    load docs and split into chunks

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain.prompts.chat import HumanMessagePromptTemplate, SystemMessagePromptTemplate
 from langchain.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import PyPDFLoader
 
 load_dotenv()
-
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+model = ChatOpenAI()
 
 template_msg = "Ton role est de repondre a des question {question}, les questions vont se baser sur des context {context}"
 system_msg_prompt = SystemMessagePromptTemplate.from_template(template_msg)
@@ -18,8 +20,10 @@ human_msg_prompt = HumanMessagePromptTemplate.from_template(
 chat_prompt_template = ChatPromptTemplate.from_messages([system_msg_prompt, human_msg_prompt])
 
 
-question = "What is LangChain?"
-context = "User guide"
-res = chat_prompt_template.format_messages(question=question,context=context)
+def load_doc(docs):
+    pass
 
-print(res)
+loaders = PyPDFLoader("CPRO.pdf")
+pages = loaders.load_and_split()
+
+print(pages)
